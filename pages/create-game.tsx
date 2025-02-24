f3a387eb641e712ccc49fb2511743931323ba8c0
martuuu <martin.navarro.dev@gmail.com>
2025-02-24 01:44:50 -0300

    Fixed bad import

--- pages/create-game.tsx
+++ pages/create-game.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { supabase } from "../supabase/client";
-import Alert from "../components/alert";
+import Alert from "../components/Alert";
 import BackButton from "@/components/BackButton";
 
 export default function CreateGame() {
